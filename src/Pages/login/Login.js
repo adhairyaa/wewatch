cb55a4b50fb0f4bf9d46280bd76deae9ca8eb7f0
Dhairya <dhairyagulati370@gmail.com>
2023-04-04 17:47:08 +0530

    update : login form

--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -1,19 +1,30 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { useAuth } from "../../Context/AuthContext";
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const { loginUserWithCredentials } = useAuth();
   return (
     <div>
       <div className="login-Form">
         <h2>Welcome to WeWatch</h2>
-        <input></input>
-        <input></input>
-        <button>Login</button>
+        <input placeholder="username"></input>
+        <input placeholder="password"></input>
+        <div className="login-guest">Login as guest</div>
+        <button
+          className="login-button"
+          onClick={() => loginUserWithCredentials(username, password)}
+        >
+          Login
+        </button>
+
         <div>
-          Dont have a account <button>SignUp</button>{" "}
+          Not a member{" "}
+          <Link to="/signup">
+            <button className="signup-Button">SignUp</button>
+          </Link>
         </div>
       </div>
     </div>
